e33252f4b630eeb555fc5ef597ed3b46a9f33b57
Matt <matnaruse@gmail.com>
2020-10-29 13:34:50 -0400

    [MN10] NON-FUNC - Created 'digitel.sql'

---
+++ SupplementalFiles/digitel.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE digitel;
+
+USE digitel;
+CREATE TABLE Customer(
+	custId SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    userName varchar(30) NOT NULL,
+    password varchar(30) NOT NULL,
+    firstname varchar(30) NOT NULL,
+    lastname varchar(30) NOT NULL,
+    address varchar(30) NOT NULL,
+    city varchar(30) NOT NULL,
+    postalCode varchar(30) NOT NULL,
+    country varchar(30) NOT NULL
+);
+
+CREATE TABLE Product(
+	productId SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    brandName varchar(30) NOT NULL,
+    modelName varchar(30) NOT NULL,
+    price double NOT NULL
+);
+
+
+-- Changed name from "Order" to "Orders" due to keyword conflict
+CREATE TABLE Orders(
+	orderId SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    custId SMALLINT,
+    productId SMALLINT,
+    quantity INT NOT NULL,
+    deliveryDate DATE NOT NULL,
+    orderStatus varchar(30),
+	foreign key (custId) REFERENCES Customer(custId) ON DELETE CASCADE,
+    foreign key (productId) REFERENCES Product(productId) ON DELETE CASCADE
+);
--- TrentMinia_MatthewNaruse_COMP303_Assignment2/src/comp303/a2/dbconn/DBGenerator.java
+++ TrentMinia_MatthewNaruse_COMP303_Assignment2/src/comp303/a2/dbconn/DBGenerator.java
@@ -8,10 +8,10 @@
 
 package comp303.a2.dbconn;
 
-import java.sql.DriverManager;
-
-import com.mysql.jdbc.Connection;
 
 public final class DBGenerator {
 	
+	public static void main(String[] args) {
+		
+	}
 }
